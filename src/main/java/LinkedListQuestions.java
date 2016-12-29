7e1d7603e298dbba085843e3c4d0317c1755d4ae
Neeraj <neeraj.reachme@gmail.com>
2016-12-28 18:35:33 -0800

    From paper solutions that were typed in to IDE solutions that work. A diff would show exactly how I can improve my on paper solutions.

--- src/main/java/LinkedListQuestions.java
+++ src/main/java/LinkedListQuestions.java
@@ -1,5 +1,8 @@
 /**
- * Created by nickamac on 12/27/16.
+ * Created by Neeraj Asrani on 12/27/16.
+ * This set of Linked List questions was received as a handout in a Tech interview meetup.
+ * All problems are solved, Method names are self explanatory to the problems being solved.
+ * In addition you will see java doc comments in the comments section
  */
 class LinkedList {
 
@@ -24,14 +27,16 @@ class LinkedList {
         }
         Node fast_ptr = head;
         Node slow_ptr = head;
-        while (slow_ptr != null) {
-            fast_ptr = fast_ptr.next.next;
-            slow_ptr = slow_ptr.next;
-        }
-        if (slow_ptr == null) {
-            return false;
-        } else if (fast_ptr == slow_ptr) {
-            return true;
+        while (true) {
+            if (fast_ptr !=null && fast_ptr.next != null) {
+                fast_ptr = fast_ptr.next.next;
+                slow_ptr = slow_ptr.next;
+            } else {
+                break;
+            }
+            if (fast_ptr==slow_ptr) {
+                return true;
+            }
         }
         return false;
     }
@@ -63,9 +68,14 @@ class LinkedList {
         }
         Node fast_ptr = head;
         Node slow_ptr = head;
-        while (fast_ptr.next != null || fast_ptr.next.next != null) {
-            fast_ptr = fast_ptr.next.next;
-            slow_ptr = slow_ptr.next;
+        while (true) {
+            if (fast_ptr.next!=null && fast_ptr.next.next!=null) {
+                fast_ptr = fast_ptr.next.next;
+                slow_ptr = slow_ptr.next;
+            }
+            else {
+                break;
+            }
         }
         return slow_ptr.value;
     }
@@ -81,27 +91,27 @@ class LinkedList {
             return null;
         }
         int hops_until_null = 0;
-        Node index_th_ptr = head;
-        Node prev_index_th_ptr = head;
+        Node index_ptr = head;
+        Node prev_to_index_ptr = head;
         Node current_ptr = head;
         while (current_ptr != null) {
-            while (current_ptr == null || hops_until_null <= index) {
+            while (current_ptr != null && hops_until_null < index) {
                 current_ptr = current_ptr.next;
                 hops_until_null++;
             }
             if (hops_until_null == index) {
-                prev_index_th_ptr = index_th_ptr;
-                index_th_ptr = current_ptr;
+                prev_to_index_ptr = index_ptr;
+                index_ptr = current_ptr;
                 hops_until_null = 0;
             } else {
                 break;
             }
         }
-        if (prev_index_th_ptr == head && index_th_ptr == head) { // edge case if n > size of Linked List
+        if (prev_to_index_ptr == head && index_ptr == head) { // edge case if n > size of Linked List
             return null;
         }
-        current_ptr = prev_index_th_ptr;
-        for (int i = 0; i <= hops_until_null; i++) {
+        current_ptr = prev_to_index_ptr;
+        for (int i = 0; i < hops_until_null; i++) {
             current_ptr = current_ptr.next;
         }
         return current_ptr.value;
@@ -112,20 +122,26 @@ class LinkedList {
      * Add a method to delete first node in the linkedList
      * Add a method to delete last node in the LinkedList
      * Add a method to delete node at index
+     * @param index
      */
     void deleteNodeAt(int index) { // assuming 0 "zero" based subsequence of index
         if (head == null) {
             return;
         }
         Node fast_ptr = head;
-        Node prev_fast_ptr = head;
-        Node prev_prev_fast_ptr = head;
+        Node prev_to_fast_ptr = head;
+        Node prev_to_prev_to_fast_ptr = head;
         Node slow_ptr = head;
         int c = 0;
-        while (fast_ptr.next != null || fast_ptr.next.next != null || c >= index) {
+
+        if (index==0) { // if index = 0
+            head = head.next;
+        }
+        while (fast_ptr.next != null && fast_ptr.next.next != null && c < index) {
+//            if (fast_ptr.next !=null && fast_ptr.next!=null && fast_ptr.next.next!=null)
+            prev_to_prev_to_fast_ptr = fast_ptr;
+            prev_to_fast_ptr = fast_ptr.next;
             fast_ptr = fast_ptr.next.next;
-            prev_fast_ptr = fast_ptr.next;
-            prev_prev_fast_ptr = fast_ptr;
             c += 2;
         }
         if (c == index) {
@@ -134,16 +150,15 @@ class LinkedList {
             }
 
             if (fast_ptr == tail) { // edge case when fast_ptr is the last element
-                prev_fast_ptr.next = null;
-                tail = prev_fast_ptr;
+                prev_to_fast_ptr.next = null;
+                tail = prev_to_fast_ptr;
             }
-            prev_fast_ptr.next = null;
+            prev_to_fast_ptr.next = fast_ptr.next;
             fast_ptr = null; // For GC
         } else { // c > index
-            prev_prev_fast_ptr.next = fast_ptr;
-            prev_fast_ptr = null;
+            prev_to_prev_to_fast_ptr.next = fast_ptr;
+            prev_to_fast_ptr = null;
         }
-
     }
 
     void insertBeforeHead(String value) {
@@ -191,16 +206,40 @@ class LinkedList {
     }
 
     void initializeLinkedList() {
+        // A cool way to initialize that makes code a little unreadable
         this.head = new Node(new Node(new Node(new Node(new Node(null, "e"), "d"), "c"), "b"), "a");
-    }
+        Node a = new Node (null, "a");
+        Node b = new Node (null, "b");
+        Node c = new Node (null, "c");
+        Node d = new Node (null, "d");
+        Node e = new Node (null, "e");
+        this.head=a; // head points to beignning of list node "a"
+        a.next=b;
+//        b.next=c;
+//        c.next=b;
+//        d.next=e; // node with cycle
 
-    void initializeTail() {
         Node newHead = head;
 
         while (newHead.next != null) {
             newHead = newHead.next;
         }
         tail = newHead;
+
+
+    }
+
+    void initializeLinkedListWithCycle() {
+        Node a = new Node (null, "a");
+        Node b = new Node (null, "b");
+        Node c = new Node (null, "c");
+        Node d = new Node (null, "d");
+        Node e = new Node (null, "e");
+        this.head=a; // head points to beignning of list node "a"
+        a.next=b;
+        b.next=c;
+        c.next=b;
+        d.next=e; // node with cycle
     }
 
     void print() {
@@ -214,7 +253,7 @@ class LinkedList {
             newHead = newHead.next;
         }
         result.append(newHead.value);
-        System.out.print(result);
+        System.out.println(result);
     }
 
 }
@@ -222,13 +261,38 @@ class LinkedList {
 public class LinkedListQuestions {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
+        LinkedList linkedListWithCycle = new LinkedList();
+
+        // Linked List With Cycle
+        linkedListWithCycle.initializeLinkedListWithCycle();
+        System.out.println("isCyclePresent = [" + linkedListWithCycle.isCyclePresent() + "]");
+
+        // Linked List
         linkedList.initializeLinkedList();
-        linkedList.initializeTail();
         linkedList.insertBeforeHead("ba");
         linkedList.insertBeforeHead("bba");
         linkedList.insertAfterTail("ae");
         linkedList.insertAfterTail("af");
+        System.out.println("isCyclePresent = [" + linkedList.isCyclePresent() + "]");
         linkedList.print();
+        System.out.println("middle node with a even numbered LL size = [" + linkedList.findMiddleNode() + "]");
+        linkedList.insertAfterTail("aaf");
+        linkedList.insertAfterTail("aaaf");
+        linkedList.insertAfterTail("aaaaf");
+        linkedList.insertBeforeHead("bbbf");
+        linkedList.insertBeforeHead("cbbf");
+        linkedList.print();
+        System.out.println("middle node with a odd numbered LL size = [" + linkedList.findMiddleNode() + "]");
+        System.out.println("11th fron end of LL size = [" + linkedList.findNthNodeFromEnd(11) + "]");
+        System.out.println("Deleting 5th node of LL, here it looks like after deletion");
+        linkedList.deleteNodeAt(5);
+        linkedList.print();
+        linkedList.initializeLinkedList();
+        linkedList.insertAfterTail("c");
+        linkedList.insertAfterTail("d");
+        linkedList.print();
+        System.out.println("middle node with a even numbered LL size = [" + linkedList.findMiddleNode() + "]");
+        System.out.println("4th fron end of LL size = [" + linkedList.findNthNodeFromEnd(4) + "]");
     }
 }
 
