bb4ade463e8802343abe2ed03c736b486c13e65e
Neeraj <neeraj.reachme@gmail.com>
2017-01-03 21:01:38 -0800

    Created example unit test cases

---
+++ src/main/java/PracticeTest.java
@@ -0,0 +1,93 @@
+/**
+ * Created by nickamac on 1/3/17.
+ */
+import com.sun.tools.javac.util.Assert;
+import org.junit.*;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.hamcrest.collection.IsEmptyCollection;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PracticeTest {
+    // Run once, e.g. Database connection, connection pool
+    @BeforeClass
+    public static void runOnceBeforeClass() {
+        System.out.println("@BeforeClass - runOnceBeforeClass");
+    }
+
+    // Run once, e.g close connection, cleanup
+    @AfterClass
+    public static void runOnceAfterClass() {
+        System.out.println("@AfterClass - runOnceAfterClass");
+    }
+
+    // Should rename to @BeforeTestMethod
+    // e.g. Creating an similar object and share for all @Test
+    @Before
+    public void runBeforeTestMethod() {
+        System.out.println("@Before - runBeforeTestMethod");
+    }
+
+    // Should rename to @AfterTestMethod
+    @After
+    public void runAfterTestMethod() {
+        System.out.println("@After - runAfterTestMethod");
+    }
+
+    @Test
+    public void test_method_1() {
+        System.out.println("@Test - test_method_1");
+    }
+
+    @Test
+    public void test_method_2() {
+        System.out.println("@Test - test_method_2");
+    }
+
+    @Test
+    public void testAssertList() {
+
+        List<String> actual = Arrays.asList("a", "b", "c");
+        List<String> expected = Arrays.asList("a", "b", "c");
+
+        System.out.println("@Test - testAssertList");
+
+        //All passed / true
+
+        //1. Test equal.
+        assertThat(actual, is(expected));
+
+        //2. If List has this value?
+        assertThat(actual, hasItems("b"));
+
+        //3. Check List Size
+        assertThat(actual, hasSize(3));
+
+        assertThat(actual.size(), is(3));
+
+        //4.  List order
+
+        // Ensure Correct order
+        assertThat(actual, contains("a", "b", "c"));
+
+        // Can be any order
+        assertThat(actual, containsInAnyOrder("c", "b", "a"));
+
+        //5. check empty list
+        assertThat(actual, not(IsEmptyCollection.empty()));
+
+        assertThat(new ArrayList<>(), IsEmptyCollection.empty());
+
+    }
+
+}
